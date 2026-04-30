e0f1cb497ceeda548afddf07b6b37a4204fe3785
adpthegreat <adpthegreatanddefiant@gmail.com>
2026-04-30 07:16:47 +0100

    chore: removed comments and unused imports, cleaned tests

--- plankc/sir/crates/passes/src/optimizations/switch_lowering.rs
+++ plankc/sir/crates/passes/src/optimizations/switch_lowering.rs
@@ -1,30 +1,12 @@
-use crate::{AnalysesStore, Pass, analyses::Predecessors};
-use plank_core::IncIterable;
-use sir_data::{BasicBlock, BasicBlockId, Branch, Control, EthIRProgram, LocalIdx, Span, Switch};
+use crate::{AnalysesStore, Pass};
+use plank_core::Idx;
+use sir_data::{Branch, Control, EthIRProgram, Switch};
 
 #[derive(Default)]
 pub struct SwitchLowering;
 
 impl Pass for SwitchLowering {
-    fn run(&mut self, program: &mut EthIRProgram, store: &AnalysesStore) {
-        // switch (x) {
-        //     case 0: A;
-        //     default: B;
-        // }
-        //to
-        // if 0 {
-        //     A CasesId (the first and only case) 
-        // } else {
-        //     B  //fallback (Option<BasicBlockId>)
-        // }
-        // if condition != 0 {
-        //     goto non_zero_target;
-        // } else {
-        //     goto zero_target;
-        // }
-        // condition        = value being tested
-        // non_zero_target  = then branch / true branch
-        // zero_target      = else branch / false branch
+    fn run(&mut self, program: &mut EthIRProgram, _store: &AnalysesStore) {
         for bb in program.basic_blocks.iter_idx() {
             match program.basic_blocks[bb].control {
                 Control::Switch(Switch { cases, fallback, condition }) => {
@@ -35,7 +17,7 @@ impl Pass for SwitchLowering {
                             let target = program.cases_bb_ids[target_idx];
                             program.basic_blocks[bb].control = Control::Branches(Branch {
                                 condition,
-                                non_zero_target: fallback, //basic block id of first and only case 
+                                non_zero_target: fallback,
                                 zero_target: target,
                             });
                         }
@@ -56,7 +38,6 @@ mod tests {
 
     #[test]
     fn test_switch_lowering() {
-
         let mut program = sir_parser::parse_or_panic(
             r#"
             fn init:
@@ -77,70 +58,21 @@ mod tests {
             EmitConfig::init_only(),
         );
 
-        for (bbi, bb) in program.basic_blocks.iter().enumerate() { 
-            println!("index: {:?}, actual block: {:?}", bbi, bb)
-            // match program.basic_blocks[bb].control {
-            //     Control::Switch(Switch { cases, fallback, .. }) => {
-            //     }
-            //     Control::Branches(Branch { condition, non_zero_target, zero_target }) => {
-            //         println!()
-            //     }
-            //     - => {}
-            // }
-        }
-        
-        let original_block_count = program.basic_blocks.len();
         let store = AnalysesStore::default();
         run_pass(&mut SwitchLowering, &mut program, &store);
 
-        for (bbi, bb) in program.basic_blocks.iter().enumerate() { 
-            println!("AFTER THE PASS:");
-            println!("index: {:?}, actual block: {:?}", bbi, bb)
-            // match program.basic_blocks[bb].control {
-            //     Control::Switch(Switch { cases, fallback, .. }) => {
-            //     }
-            //     Control::Branches(Branch { condition, non_zero_target, zero_target }) => {
-            //         println!()
-            //     }
-            //     - => {}
-            // }
+        for (bbi, bb) in program.basic_blocks.iter().enumerate() {
+            match &bb.control {
+                Control::Branches(Branch { non_zero_target, zero_target, .. }) => {
+                    assert!(
+                        non_zero_target.get() != zero_target.get(),
+                        "Block {} has branch targets",
+                        bbi
+                    );
+                }
+                _ => {}
+            }
         }
-        // assert!(
-        //     program.basic_blocks.len() > original_block_count,
-        //     "critical edge splitting should insert forwarding blocks"
-        // );
     }
 }
 
-
-
-//      Running unittests src/lib.rs (target/debug/deps/sir_passes-42a8a30451163452)
-
-// running 1 test
-
-// index: 0, actual block: BasicBlock { inputs: Span { start: LocalIdx(0), end: LocalIdx(0) }, outputs: Span { start: LocalIdx(0), end: LocalIdx(0) }, operations: Span { start: OperationIdx(0), end: OperationIdx(1) }, control: Switch(Switch { condition: LocalId(0), fallback: Some(BasicBlockId(2)), cases: CasesId(0) }) }
-
-// index: 1, actual block: BasicBlock { inputs: Span { start: LocalIdx(0), end: LocalIdx(0) }, outputs: Span { start: LocalIdx(0), end: LocalIdx(0) }, operations: Span { start: OperationIdx(1), end: OperationIdx(1) }, control: ContinuesTo(BasicBlockId(2)) }
-
-// index: 2, actual block: BasicBlock { inputs: Span { start: LocalIdx(0), end: LocalIdx(0) }, outputs: Span { start: LocalIdx(0), end: LocalIdx(0) }, operations: Span { start: OperationIdx(1), end: OperationIdx(2) }, control: LastOpTerminates }
-
-
-// AFTER THE PASS:
-
-// index: 0, actual block: BasicBlock { inputs: Span { start: LocalIdx(0), end: LocalIdx(0) }, outputs: Span { start: LocalIdx(0), end: LocalIdx(0) }, operations: Span { start: OperationIdx(0), end: OperationIdx(1) }, control: Branches(Branch { condition: LocalId(0), non_zero_target: BasicBlockId(2), zero_target: BasicBlockId(1) }) }
-
-// AFTER THE PASS:
-// index: 1, actual block: BasicBlock { inputs: Span { start: LocalIdx(0), end: LocalIdx(0) }, outputs: Span { start: LocalIdx(0), end: LocalIdx(0) }, operations: Span { start: OperationIdx(1), end: OperationIdx(1) }, control: ContinuesTo(BasicBlockId(2)) }
-
-// AFTER THE PASS:
-// index: 2, actual block: BasicBlock { inputs: Span { start: LocalIdx(0), end: LocalIdx(0) }, outputs: Span { start: LocalIdx(0), end: LocalIdx(0) }, operations: Span { start: OperationIdx(1), end: OperationIdx(2) }, control: LastOpTerminates }
-
-// test optimizations::switch_lowering::tests::test_switch_lowering ... ok
-
-// test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 108 filtered out; finished in 0.00s
-
-//      Running unittests src/lib.rs (target/debug/deps/sir_test_utils-d37c0958467bd984)
-
-// running 0 tests
-
-// test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
